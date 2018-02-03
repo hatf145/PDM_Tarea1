897aa55a7df46ef362f1e556a5036f4034ce9067
Hector Toscano <hectos145@hotmail.com>
2018-02-02 21:04:23 -0600

    Segundo Commit

--- app/src/main/java/iteso/com/sesion5/Alumno.java
+++ app/src/main/java/iteso/com/sesion5/Alumno.java
@@ -8,15 +8,6 @@ public class Alumno {
     String name, phone, degree, gender, favBook;
     Boolean sports;
 
-    Alumno(){
-        name = "";
-        phone = "";
-        degree = "";
-        gender = "";
-        favBook = "";
-        sports = true;
-    }
-
     Alumno(String name, String phone, String degree, String gender, String favBook, Boolean sports){
         this.name = name;
         this.phone = phone;
@@ -24,54 +15,6 @@ public class Alumno {
         this.gender = gender;
         this.favBook = favBook;
         this.sports = sports;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setDegree(String degree) {
-        this.degree = degree;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public void setFavBook(String favBook) {
-        this.favBook = favBook;
-    }
-
-    public void setSports(Boolean sports) {
-        this.sports = sports;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getDegree() {
-        return degree;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public String getFavBook() {
-        return favBook;
-    }
-
-    public Boolean getSports() {
-        return sports;
     }
 
     @Override
